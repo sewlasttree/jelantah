b409998583588be8eea7997fd94228e4407d7a83
sewlasttree <sew.last.tree@gmail.com>
2024-03-19 17:12:41 +0700

    add style to button & arrow down

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
    setTimeout( () => {
-      hideLoader();
-      showContent();
-   }, 2000);
+      // hideLoader();
+      // showContent();
+      showPage()
+   }, 1000);
 
-   function hideLoader() {
-      const loader = document.querySelector('#preloader');
-      loader.style.display = 'none';
-   }
+   // function hideLoader() {
+   //    const loader = document.querySelector('#preloader');
+   //    loader.style.display = 'none';
+   // }
 
-   function showContent() {
-      const content = document.querySelector('#status');
-      content.style.display = 'block';
-   }
+   // function showContent() {
+   //    const content = document.querySelector('#status');
+   //    content.style.display = 'block';
+   // }
+
+    function showPage() {
+      document.querySelector("#preloader").style.opacity = 0;
+      document.querySelector("#status").style.opacity = 1;
+    }
+
 });
